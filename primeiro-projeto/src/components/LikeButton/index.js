44c12b40f30cc0e016bc9b14726dad2b72a87379
evacosta <evacosta.if@gmail.com>
2025-10-20 17:54:37 -0300

    exercicios

---
+++ primeiro-projeto/src/components/LikeButton/index.js
@@ -0,0 +1,5 @@
+// Ele deve ter um state chamado likes, começando em 0.
+
+// Ele deve exibir um botão que, ao ser clicado, incrementa o likes.
+
+// Ele deve exibir o número de likes (ex: "❤️ 0 Likes").
